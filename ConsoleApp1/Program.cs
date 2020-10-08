ec882d82cc92842ca34993c615f2f387f703a737
Agustina Notti <agustinantt@gmail.com>
2020-10-08 00:23:28 -0300

    guardar errores en txt

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp1.Errores.LanzarError;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejemplo.Prueba2 prueba2 = new Ejemplo.Prueba2(); //instancio la clase
+            prueba2.ejemplo();
+
+            prueba2.getSaludo();
+
+
+            // Class1 es static, con metodos estaticos... no necesito instanciar. Se agrega en referencias
+
+
+            try
+            {
+                Errores.LanzarError.LanzarError.lanzarError();
+            }
+            catch(Exception error)
+            {
+                Errores.CapturarError.Logger.guardarError(error.Message);
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/Prueba2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    namespace Ejemplo
+    {
+        class Prueba2
+        {
+            public void ejemplo()
+            {
+                System.Console.WriteLine("hola");
+            }
+
+            public string getSaludo()
+            {
+                return "hola";
+            }
+        }
+    }
+
+    namespace Errores
+    {
+
+        namespace CapturarError
+        {
+
+            public static class Logger
+            {
+                public static void guardarError(string error)
+                {
+
+                    string pathError = ConfigurationManager.AppSettings["pathError"];
+                    string fecha = System.DateTime.Now.ToString("yyyyMMdd");
+                    string hora = System.DateTime.Now.ToString("HH:mm:ss");
+                    //string path = "E:\\Mi unidad\\EDSA Academy\\errores.txt";
+
+                    StreamWriter sw = new StreamWriter(pathError, true);
+
+                    sw.WriteLine($"{fecha} , {hora} - {error}");
+                    sw.WriteLine("");
+
+                    sw.Flush();
+                    sw.Close();
+                }
+            } 
+
+        }
+
+        namespace LanzarError
+        {
+            public static class LanzarError
+            {
+                public static void lanzarError()
+                {
+                    throw new Exception("Error");
+                }
+            }
+
+        }
+    }
+
+
+}
